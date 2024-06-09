c16a56abb2053d62975c2e60d731a314367262af
CrystalFireSword <159522736+CrystalFireSword@users.noreply.github.com>
2024-06-09 18:24:49 +0530

    Update login_sample.py

--- login_sample.py
+++ login_sample.py
@@ -1,20 +1,12 @@
 import csv
 
 def login_check(username, password):
+
+    '''CHECKS WHETHER LOGIN CREDENTIALS ARE CORRECT'''
+    
     with open('login.csv', 'r', newline = '') as f:
         reader = csv.reader(f)
         for content in reader:
             if content[1] == username and content[2] == password:
                 return True
 
-# for x in range(5):
-#     username = input('username:')
-#     password = input('password:')
-
-#     valid = login_check(username, password)
-#     if valid:
-#         print('Login successful!')
-#     else:
-#         print('Login ID or password is incorrect.')
-
-        
