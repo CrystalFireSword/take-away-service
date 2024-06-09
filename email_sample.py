b65020fb3a70d6d2247695667b4483c4dbb606a8
CrystalFireSword <159522736+CrystalFireSword@users.noreply.github.com>
2024-06-09 18:10:56 +0530

    Update email_sample.py
    Added and removed comments

--- email_sample.py
+++ email_sample.py
@@ -1,12 +1,9 @@
-from email.mime.text import MIMEText 
-from email.mime.image import MIMEImage 
-from email.mime.application import MIMEApplication 
-from email.mime.multipart import MIMEMultipart 
-import smtplib 
-import os
 import csv 
 
 def get_order_mail_id(order_id):
+    
+    '''RETRIEVES USER'S MAIL ID AND NAME WHEN GIVEN THEIR ORDER ID '''
+    
     mail_id = ''
     name = ''
     try:
@@ -20,26 +17,3 @@ def get_order_mail_id(order_id):
     except:
         pass
     return mail_id, name
-
-
-
-def send_mail(to_address, status, o_id):
-    website = ''
-    smtp = smtplib.SMTP('smtp.gmail.com', 587) 
-    smtp.ehlo() 
-    smtp.starttls() 
-    smtp.login('anikrishofficial@gmail.com', 'fxlk qrka fzpt wwte') 
-    msg = MIMEMultipart() 
-    msg['Subject'] = f"RISHABH'S ORDER STATUS FOR {o_id}"
-    text = f'''Hi! {status}! \n You can track your order on our website using your order id. \n Order ID: {o_id} \n Website Link: {website} \n Thank you! Enjoy your meal!'''
-    msg.attach(MIMEText(text))
-    ImgFileName = r'C:\Users\aksha\food_delivery\static\rishabhs_pic.png'
-    with open(ImgFileName, 'rb') as f:
-        img_data = f.read()
-    image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
-    msg.attach(image)
-    to = to_address 
-    smtp.sendmail(from_addr="anikrishofficial@gmail.com", 
-                to_addrs=to, msg=msg.as_string()) 
-    smtp.quit()
-    print('Mail sent!')
