68e167286c9d7b55fed6761a6f5a7b0dc4fc67fc
CrystalFireSword <159522736+CrystalFireSword@users.noreply.github.com>
2024-06-09 18:07:24 +0530

    Update csv_sample.py
    Added comments

--- csv_sample.py
+++ csv_sample.py
@@ -1,14 +1,8 @@
 import csv
 from datetime import date
 
-# with open('login.csv', 'w', newline = '') as f:
-#     writer = csv.writer(f)
-#     row1 = ['userid', 'username', 'password']
-#     rows_to_be_entered = ['001', 'XYZ', 'ABC']
-#     writer.writerow(row1)
-#     writer.writerow(rows_to_be_entered)
-
 def get_menu(menu_type = 0):
+    ''' RETURNS MENU AS A NESTED LIST '''
     menu = []
     with open('menu.csv', 'r') as f:
         r = csv.reader(f)
@@ -17,20 +11,23 @@ def get_menu(menu_type = 0):
             if x[0] != '':
                 
                 if menu_type == 0:
-                    
+                    ''' MENU TO BE DISPLAYED TO USER WHILE PLACING ORDER '''    
                     if x[3] == '1':
-                        
+                        ''' CHECKING AVAILABILITY '''
                         menu.append(x[:3])
                         
                 elif menu_type == 1:
+                    ''' MENU TO BE DISPLAYED TO ADMIN FOR UPDATION '''
                     menu.append(x[:4])
                 
     return menu
 menu = get_menu()
-#print(menu)
+
 
 
 def temporary_cart(item_id_list):
+
+    ''' RETURNS THE LIST OF ORDERED ITEMS TO BE TRANSFERRED TO THE CART'''
     menu = get_menu()
     cart_data = []
     item_id_keys = [j for x in item_id_list for j in x]
@@ -43,14 +40,12 @@ def temporary_cart(item_id_list):
         return cart_data
     else:
         return -1
-    '''
-    with open('current_cart.csv', 'w', newline = '') as f:
-        writer = csv.writer(f)
-        rows_to_be_entered = cart_data
-        writer.writerows(rows_to_be_entered)
-    '''
+    
 
 def write_menu(menu_to_write):
+
+    ''' WRITES ITEMS IN THE MENU AND RELATED INFORMETION INTO THE MENU FILE'''
+    
     with open('menu.csv', 'w', newline = '') as f:
         writer = csv.writer(f)
         row_to_be_entered = ['Item_no','Item_name','Item_price','Availability']
@@ -59,6 +54,9 @@ def write_menu(menu_to_write):
         writer.writerows(rows_to_be_entered)
 
 def write_customer_data(o_id, e_id, name = ''):
+
+    ''' WRITES CUSTOMER DATA INTO THE FILE '''
+    
     with open('customer_id.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow([o_id, e_id, name])
